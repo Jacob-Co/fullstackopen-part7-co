ea4bf2f067318b452b455ad78f16d4079b019ef6
Jacobtco <jacobtco@gmail.com>
2020-09-08 09:21:02 +0800

    Created initializeBlog action creator

--- blog-list/bloglist-frontend/src/reducer/blogReducer.js
+++ blog-list/bloglist-frontend/src/reducer/blogReducer.js
@@ -1,7 +1,22 @@
 import blogService from '../services/blogs';
 
-const blogReducer = (state = [], action) => {
+export const initializeBlog = () => {
+  return async (dispatch) => {
+    const blogs = await blogService.getAll();
+    dispatch({
+      type: 'INIT_BLOG',
+      data: blogs
+    });
+  };
+};
 
-}
+const blogReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'INIT_BLOG':
+      return action.data;
+      default:
+        return state;
+  }
+};
 
 export default blogReducer;
