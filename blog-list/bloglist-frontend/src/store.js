7d3baa541a5031e04b197f7557569e65decbfff9
Jacobtco <jacobtco@gmail.com>
2020-09-08 09:52:52 +0800

    Replaced react-thunk with redux-thunk

--- blog-list/bloglist-frontend/src/store.js
+++ blog-list/bloglist-frontend/src/store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'react-thunk';
+import thunk from 'redux-thunk';
 
 import blogReducer from './reducer/blogReducer';
 
