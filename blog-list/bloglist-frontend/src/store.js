abf3be6c9626f5cc432503a069147ff542f32413
Jacobtco <jacobtco@gmail.com>
2020-09-08 16:37:27 +0800

    Added userReducer to redux store

--- blog-list/bloglist-frontend/src/store.js
+++ blog-list/bloglist-frontend/src/store.js
@@ -3,9 +3,11 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 import blogReducer from './reducer/blogReducer';
+import userReducer from './reducer/userReducer';
 
 const reducer = combineReducers({
-  blogs: blogReducer
+  blogs: blogReducer,
+  user: userReducer
 })
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
