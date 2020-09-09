25fc3f8ed33c8ff87e29d164cc177b35473daeff
Jacobtco <jacobtco@gmail.com>
2020-09-09 18:23:28 +0800

    Placed notification reducer into redux store

--- blog-list/bloglist-frontend/src/store.js
+++ blog-list/bloglist-frontend/src/store.js
@@ -4,10 +4,12 @@ import thunk from 'redux-thunk';
 
 import blogReducer from './reducer/blogReducer';
 import userReducer from './reducer/userReducer';
+import notificationReducer from './reducer/notificationReducer'
 
 const reducer = combineReducers({
   blogs: blogReducer,
-  user: userReducer
+  user: userReducer,
+  notification: notificationReducer
 })
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
