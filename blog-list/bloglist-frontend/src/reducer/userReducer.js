98825cfa2f1cd6c2af809f53d0c763e23b47ea41
Jacobtco <jacobtco@gmail.com>
2020-09-09 15:35:58 +0800

    userReducer, added a setUser action creator

--- blog-list/bloglist-frontend/src/reducer/userReducer.js
+++ blog-list/bloglist-frontend/src/reducer/userReducer.js
@@ -11,9 +11,20 @@ export const login = (credentials) => {
   };
 }
 
+export const setUser = (user) => {
+  return async (dispatch) => {
+    dispatch({
+      type: 'SET_USER',
+      data: user
+    })
+  }
+}
+
 const userReducer = (state = null, action) => {
   switch (action.type) {
     case 'LOGIN':
+      return action.data;
+    case 'SET_USER':
       return action.data;
     default:
       return state;
