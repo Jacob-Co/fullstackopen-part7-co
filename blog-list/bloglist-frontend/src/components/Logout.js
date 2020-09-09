f927b481ec6eb98dc2a0ea3527979b59b17e26f7
Jacobtco <jacobtco@gmail.com>
2020-09-09 18:53:58 +0800

    Logout comp, removed createNotification action creator

--- blog-list/bloglist-frontend/src/components/Logout.js
+++ blog-list/bloglist-frontend/src/components/Logout.js
@@ -2,18 +2,12 @@ import React from 'react';
 import { useDispatch } from 'react-redux'
 
 import { logout } from '../reducer/userReducer';
-import { createNotification } from '../reducer/notificationReducer';
 
 const Logout = () => {
   const dispatch = useDispatch();
 
-  const handleClick = () => {
-    dispatch(createNotification('Logged out', 'warning'));
-    dispatch(logout());
-  }
-
   return (
-    <button onClick={handleClick}>logout</button>
+    <button onClick={() => dispatch(logout())}>logout</button>
   )
 }
 
