bfea4f5f9d433317f888eaff4430ebec1e3a0206
Jacobtco <jacobtco@gmail.com>
2020-09-09 18:26:57 +0800

    App comp, remove notif funcs, added greeting when there is a web token

--- blog-list/bloglist-frontend/src/App.js
+++ blog-list/bloglist-frontend/src/App.js
@@ -10,31 +10,24 @@ import Logout from './components/Logout';
 
 // reducer
 import { setUser } from './reducer/userReducer';
+import { createNotification } from './reducer/notificationReducer';
 
 const App = () => {
   const dispatch = useDispatch();
   const user = useSelector(state => state.user);
-  
-  const [message, setMessage] = useState(null);
-  const [notifType, setNotifType] = useState(null);
 
   useEffect(() => {
     const localUser = window.localStorage.getItem('localBlogAppUser');
     if (localUser) {
       const transformedUser = JSON.parse(localUser);
       dispatch(setUser(transformedUser));
+      dispatch(createNotification(`Welcome ${transformedUser.name}`, 'success'))
     }
   }, [dispatch]);
 
-  const setNotification = (message, type) => {
-    setMessage(message);
-    setNotifType(type);
-    setTimeout(() => setMessage(null), 5000);
-  };
-
   return (
     <>
-      <Notification message={message} type={notifType} />
+      <Notification />
       {
         user === null
           ? <Login />
