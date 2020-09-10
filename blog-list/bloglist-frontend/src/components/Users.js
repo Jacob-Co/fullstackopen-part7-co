5bc78c89b56b25ce70309dc6a98f09e7415dcc6d
Jacobtco <jacobtco@gmail.com>
2020-09-10 11:47:13 +0800

    Users comp now contians switch and routes, where if the route is /user/:id will show more info about that user

--- blog-list/bloglist-frontend/src/components/Users.js
+++ blog-list/bloglist-frontend/src/components/Users.js
@@ -1,11 +1,23 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
+import {
+  Link,
+  Switch,
+  Route,
+  useRouteMatch
+} from 'react-router-dom'
 
 import { initializeUsers } from '../reducer/userReducer'
+import User from './User';
+
 
 const Users = () => {
   const users = useSelector(state => state.users)
   const dispatch = useDispatch();
+  const userMatch = useRouteMatch('/user/:id');
+  const user = userMatch
+    ? users.find(user => user.id === (userMatch.params.id))
+    : null;
 
   useEffect(() => {
     dispatch(initializeUsers());
@@ -13,18 +25,26 @@ const Users = () => {
   
   return (
     <div>
-      <h2>Users</h2>
-      <table>
-        <thead>
-          <tr><th></th><th>blogs created</th></tr>
-        </thead>
-        <tbody>
-          {users.map(user => <tr key={user.id}>
-            <td>{user.username}</td>
-            <td>{user.blogs.length}</td>
-          </tr>)}
-        </tbody>
-      </table>
+      <Switch>
+        <Route path="/user/:id">
+          <User user={user} />
+        </Route>
+
+        <Route path="/">
+            <h2>Users</h2>
+          <table>
+            <thead>
+              <tr><th></th><th>blogs created</th></tr>
+            </thead>
+            <tbody>
+              {users.map(user => <tr key={user.id}>
+                <td><Link to={`/user/${user.id}`}>{user.username}</Link></td>
+                <td>{user.blogs.length}</td>
+              </tr>)}
+            </tbody>
+          </table>
+        </Route>
+      </Switch>
     </div>
   )
 }
