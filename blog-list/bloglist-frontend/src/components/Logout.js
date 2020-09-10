df3a4eceadde889558312943dbcdb932f57777e8
Jacobtco <jacobtco@gmail.com>
2020-09-10 12:15:45 +0800

    Logout comp, added history push to home route when logging out

--- blog-list/bloglist-frontend/src/components/Logout.js
+++ blog-list/bloglist-frontend/src/components/Logout.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 
 import { logout } from '../reducer/tokenReducer';
 
 const Logout = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleLogout = () => {
+    dispatch(logout())
+    history.push('/')
+  }
 
   return (
-    <button onClick={() => dispatch(logout())}>logout</button>
+    <button onClick={handleLogout}>logout</button>
   )
 }
 
