8d1b7fdc317096cbe52ac373f56c9435bc0844e9
Jacobtco <jacobtco@gmail.com>
2020-09-11 19:08:19 +0800

    CommentList comp, added CommentForm

--- blog-list/bloglist-frontend/src/components/CommentList.js
+++ blog-list/bloglist-frontend/src/components/CommentList.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import CommentForm from './CommentForm';
+
 const CommentList = ({blog}) => {
   return (
     <div>
       <h3>comments</h3>
+      <CommentForm blog={blog} />
       <ul>
         {blog.comments.map(comment => <li key={comment.id}>{comment.comment}</li>)}
       </ul>
